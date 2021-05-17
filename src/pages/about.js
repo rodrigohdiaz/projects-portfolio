ffadbfc904dc775889ee7d9aaa0e9fb5a71f405a
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-05-17 17:50:59 +1200

    add links to pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const About = () => {
     return (
         <main>
+        <Link to='/'>Home</Link>
         <title>About</title>
         <h1>I'm Rodrigo Díaz</h1>
         <p>Reach out if you have a question or if you'd like to chat.</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <main>
+      <Link to='/about'>About</Link>
       <title>Home Page</title>
       <h1>Welcome to my site!</h1>
       <p>This page is to show my projects.</p>
