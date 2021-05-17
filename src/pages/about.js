da85a3920edd2e2c0d817fee8e93ab514234e966
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-05-17 17:09:56 +1200

    make about and index pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const About = () => {
+    return (
+        <main>
+        <title>About</title>
+        <h1>I'm Rodrigo Díaz</h1>
+        <p>Reach out if you have a question or if you'd like to chat.</p>
+      </main>
+    )
+}
+
+export default About
