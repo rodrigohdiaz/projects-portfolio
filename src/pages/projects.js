0d08c616a120007b616e63b44778d23075bb33cc
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-05-17 19:26:10 +1200

    add styles

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const Projects = () => {
+    return (
+        <Layout pageTitle="Projects">
+                    <p>Have a look at what I have been workin on!</p>
+        </Layout>
+    )
+}
+
+export default Projects
