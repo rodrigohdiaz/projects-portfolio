973859f3649c9ab556823e7ade2287b25f4ea4b9
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-05-18 16:47:14 +1200

    fixed typo on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
     return (
         <Layout pageTitle="Contact">
                 <h1>Need to reach me?</h1>
-                    <p>You can do so in ano of my social media!</p>
+                    <p>You can do so in any of my social media!</p>
         <Media></Media>
       </Layout>
     )
