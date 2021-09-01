d27048b1150aa74854c5a59ba0a07004202217c5
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-09-01 16:48:03 +1200

    add a blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import Media from '../components/media'
+
+
+const Blog = () => {
+    return (
+        <Layout pageTitle="Blog">
+                <h3>A penny for my thoughts? have them for free if you'd like</h3>
+                <p className="plainText center">
+                  <form>
+                      <label>
+                        
+                      </label>
+                  </form>
+                  
+                </p>
+        <Media></Media>
+        </Layout>
+    )
+}
+
+export default Blog
