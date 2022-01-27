b014c790a646724b988d10ea0e14ba8915f05079
Rodrigo Diaz <rodrigohdiaz@gmail.com>
2022-01-27 17:15:46 +1300

    add s.t.a.r.f. to kapture and draw app on projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -21,12 +21,12 @@ const Projects = () => {
                             Final project from Enspiral Dev Academy.  The aim of the project is to 
                             encourage interaction with nature by capturing images/descriptions of 
                             native birds.<br></br><br></br>
-                            <strong>The situation:</strong>
+                            <strong>The situation:</strong><br/>
                             Develop and present the final project for Enspiral Dev Academy’s 
                             bootcamp, starting from an idea, problem statement, user stories, 
                             elements needed to make these stories, develop the solution using 
                             our existing capabilities.<br></br><br></br>
-                            <strong>The task:</strong>
+                            <strong>The task:</strong><br/>
                             We had 5 days and we were a team of 4 people, 2 front end and 2 back 
                             end developers.  I was working on the front end. We developed style 
                             standards that were to be for the overall design to be consistent 
@@ -37,7 +37,7 @@ const Projects = () => {
                             that the style was consistent and make sure that every element was in a 
                             proper position and that the user experience was optimised with proper 
                             design of elements on each page/card.<br></br><br></br>
-                            <strong>Actions taken</strong>
+                            <strong>Actions taken</strong><br/>
                             We had regular stand ups, code reviews and check ins.  We also fostered an 
                             open and collaborative team environment by have an open desk policy, there 
                             was no stupid question and every problem had to be discussed with the full 
@@ -46,7 +46,7 @@ const Projects = () => {
                             were to be earnt by the user depending on which birds were spotted, we 
                             decided to implement this feature to add a sense of achievement and 
                             encourage the user to try to spot more birds.<br></br><br></br>
-                            <strong>Result:</strong>
+                            <strong>Result:</strong><br/>
                             The final project can be seen in
                             <a href="https://kapturebyslick.herokuapp.com/profile" target="_blank" rel="noopener noreferrer">Kapture</a>.
                             We reached our MVP and were able to start some of the stretch goals.  
@@ -54,7 +54,7 @@ const Projects = () => {
                             the page loader and tests.  The animation for the landing page was vital to the 
                             presentation of the project. Overall I reached the goals I had as front end 
                             developer in this project.<br></br><br></br>
-                            <strong>How this made me feel?</strong>
+                            <strong>How this made me feel?</strong><br/>
                             The final result made us, and me, very happy. It taught me important skills 
                             of working collaboratively with others, to give and receive support from my 
                             team members.  Another big lesson was the importance of keeping morale up.  
@@ -62,7 +62,7 @@ const Projects = () => {
                             pressured environment.  We focused on morale by having constant check ins 
                             with each other and our own ritual to say that we were a ‘super hero’ which 
                             boosted our morale and confidence, and even made the other teams laugh - 
-                            boosting their spirits too.
+                            boosting their spirits too.<br></br><br></br><hr/>
                             </p>   
                             </div>
                         </div>
@@ -70,6 +70,42 @@ const Projects = () => {
                                 <Link to="/draw" target="_blank" rel="noopener noreferrer">
                             <div className='circle'>
                             </div><p className='text'>Draw app</p></Link>  
+                            <div className="parragraph">
+                            <p className="plainText center">
+                            Personal project that I have been working on, it is a real life drawing 
+                            canvas. The main goal is to every person that is on the app will be able 
+                            to see what the main user is drawing. I think that this app could have a 
+                            great implementation for presentations, specially in classrooms, when the 
+                            teacher is drawing a sketch or a diagram to teach, so students can pay full 
+                            attention without worrying to copy the diagram and just ‘screenshot’ it at 
+                            the end. <br></br><br></br>
+                            <strong>The situation:</strong><br/>
+                            I want to have a personal project that I can be working on on my free time, 
+                            if I have any ;P . 
+                            This particular app I started using p5.js but I am in the process to pass 
+                            it into three.js as I have been 
+                            working a little bit with this.<br></br><br></br>
+                            <strong>The task:</strong><br/>
+                            Create a canvas that will allow the user to draw anything, and then, through 
+                            sockets.io allow other users to be able to see what the main user is drawing in 
+                            real time.<br></br><br></br>
+                            <strong>Actions taken</strong><br/>
+                            I created a first app, with works to a point, only the main user can draw and 
+                            if someone else enters the app, will have an empty canvas to draw. 
+                            At the moment, this app works with p5.js, which is a library for javascript which 
+                            allows to create this drawing app<br></br><br></br>
+                            <strong>Result:</strong><br/>
+                            At the moment, there is a working drawing app, it is simple and does the main job, 
+                            which is to draw on a canvas.  Still need to work on the socket.io part and also, 
+                            since I have been learning three.js which is a cross browser javascript library that 
+                            allows to draw, animate and create elements in 2D and 3D, I want to work again this 
+                            project but in three.js.<br></br><br></br>
+                            <strong>How this made me feel?</strong><br/>
+                            The current result made me feel like I can accomplish anything, it is a great sensation 
+                            to see live and working what you imagined.  There is still job to do, and I’m looking 
+                            forward to have a final full working project.<br></br><br></br><hr/>
+                            </p>   
+                            </div>
                         </div>
                         <div className='circles'>
                                 <a href="https://github.com/horoeka-2020/NOWhere" target="_blank" rel="noopener noreferrer">
