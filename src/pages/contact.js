79ddc9457987cf5696a7e0ebabcabbc97b5f10ab
Rodrigo Diaz <rodrigohdiaz@gmail.com>
2022-01-28 16:36:20 +1300

    adds image on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Media from '../components/media'
+import contact from '../images/contactMe.png'
+import kapture from "../images/kapture.png"
 
 const Contact = () => {
     return (
         <Layout pageTitle="Contact">
                 <h1>Need to reach me?</h1>
                     <p>You can do so in any of my social media!</p>
+                    <img src={contact} 
+                         className="imageContact" 
+                         alt="contact"></img>
         <Media></Media>
       </Layout>
     )
